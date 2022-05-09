1915f4eab8026af2b3d54f2ad9e8005107f53439
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-05-09 17:18:24 -0400

    Task 4.8

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -4,34 +4,38 @@ import "./WelcomeScreen.css";
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ? (
     <div className="WelcomeScreen">
-      <h1>Welcome to the Meet app</h1>
-      <h4>
-        Log in to see upcoming events around the world for full-stack developers
-      </h4>
-      <div className="button_cont" align="center">
-        <div className="google-btn">
-          <div className="google-icon-wrapper">
-            <img
-              className="google-icon"
-              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
+      <div className="Welcome">
+        <h1>Welcome to the Meet app</h1>
+        <h4>
+          Log in to see upcoming events around the world for full-stack
+          developers
+        </h4>
+        <div className="button_cont" align="center">
+          <div className="google-btn">
+            <div className="google-icon-wrapper">
+              <img
+                className="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
 o.svg"
-              alt="Google sign-in"
-            />
+                alt="Google sign-in"
+              />
+            </div>
+            <button
+              onClick={() => {
+                props.getAccessToken();
+              }}
+              rel="nofollow noopener"
+              className="btn-text"
+            >
+              <b>Sign in with google</b>
+            </button>
           </div>
-          <button
-            onClick={() => {
-              props.getAccessToken();
-            }}
-            rel="nofollow noopener"
-            className="btn-text"
-          >
-            <b>Sign in with google</b>
-          </button>
         </div>
       </div>
       <a
         href="https://courtneyyatteau.github.io/meet/privacy.html"
-        rel="nofollow noopener"
+        rel="nofollow noopener noreferrer"
+        target="_blank"
       >
         Privacy policy
       </a>
