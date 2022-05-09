3cd8ed5c82a04e84f37ea694e7a6194619c0426a
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-05-09 18:33:47 -0400

    Warning Alert Added

--- src/Alert.js
+++ src/Alert.js
@@ -47,4 +47,12 @@ class ErrorAlert extends Alert {
   };
 }
 
-export { InfoAlert, ErrorAlert };
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = "red";
+    this.fontSize = "16px";
+  }
+}
+
+export { InfoAlert, ErrorAlert, WarningAlert };
